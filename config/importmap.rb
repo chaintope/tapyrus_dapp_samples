4e4cc58d3826bd4ea3056e6dcb51cb0427a791dd
Jun Okumura <okumura@haw.co.jp>
2022-06-15 16:21:50 +0900

    bootstrap導入

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,5 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
+pin "popper", to: "popper.js", preload: true # @2.11.5
+pin "bootstrap", to: "bootstrap.min.js", preload: true # @5.1.3
