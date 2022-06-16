8e94f9968894015644a09eb346c51357676e8449
Jun Okumura <okumura@haw.co.jp>
2022-06-16 15:52:02 +0900

    Timestamp一覧の表示項目を整理しshowへのリンクを追加
    Showページに取得できるパラメータ全てを表示するようにレイアウト

--- app/controllers/timestamps_controller.rb
+++ app/controllers/timestamps_controller.rb
@@ -2,4 +2,8 @@ class TimestampsController < ApplicationController
   def index
     @timestamps = Kaminari.paginate_array(TapyrusApi.get_timestamps).page(params[:page])
   end
+
+  def show
+    @timestamp = TapyrusApi.get_timestamp(params[:id])
+  end
 end
