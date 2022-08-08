d9edfa4331eb5de92319644bc54e430cdce94c89
Yukishige Nakajo <nakajo@chaintope.com>
2022-08-08 12:32:56 +0900

    Change to write access_token instead load, and load cert from p12 file.

--- lib/utils/tapyrus_api.rb
+++ lib/utils/tapyrus_api.rb
@@ -1,4 +1,5 @@
 # ワークで実装
+ACCESS_TOKEN = "ここにアクセストークンを記入してください"
 TAPYRUS_API_ENDPOINT_URL = "ここにURLを記入してください"
 
 class TapyrusApi
@@ -49,7 +50,7 @@ class TapyrusApi
   attr_reader :connection, :access_token, :url
 
   def initialize
-    load_access_token
+    @access_token = ACCESS_TOKEN
     @url = TAPYRUS_API_ENDPOINT_URL
     raise TapyrusApi::UrlNotFound, "接続先URLが正しくありません" if URI::DEFAULT_PARSER.make_regexp.match(@url).blank?
     @connection ||= Faraday.new(@url) do |builder|
@@ -62,30 +63,21 @@ class TapyrusApi
 
   private
 
-  def load_access_token
-    if File.exist?(Rails.root.join("access_token.txt"))
-      @access_token = File.read(Rails.root.join("access_token.txt"))
-    else
-      Rails.logger.warn("Not Found Access Token")
-      @access_token = nil
-    end
-  end
-
   def client_cert
-    OpenSSL::X509::Certificate.new File.read(client_pem_path)
+    OpenSSL::PKCS12.new(load_client_cert_p12, 'b3workshop').certificate
   rescue Errno::ENOENT => e
     Rails.logger.error(e)
     raise TapyrusApi::FileNotFound, 'クライアント証明書がありません'
   end
 
   def client_key
-    OpenSSL::PKey.read File.read(client_pem_path)
+    OpenSSL::PKCS12.new(load_client_cert_p12, 'b3workshop').key
   rescue Errno::ENOENT => e
     Rails.logger.error(e)
     raise TapyrusApi::FileNotFound, 'クライアント証明書がありません'
   end
 
-  def client_pem_path
-    Rails.root.join("tapyrus_api_client.pem")
+  def load_client_cert_p12
+    File.read(Rails.root.join("tapyrus_api_client_cert_2022-07-26.p12"))
   end
 end
